0b8e34e6588644385750fb53092e34497ac8ad6e
km841 <56809638+km841@users.noreply.github.com>
2021-10-25 21:05:05 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_3/Q1_Tower/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Tower.h"
+
+using namespace std;
+
+int main()
+{
+    Tower myTower;
+    Tower seoulTower(100);
+    cout << "높이는 " << myTower.getHeight() << "미터" << endl;
+    cout << "높이는 " << seoulTower.getHeight() << "미터" << endl;
+}
+
