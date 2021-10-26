f44f236bd289d65227ba91f3606cb00ab0ee3366
km841 <56809638+km841@users.noreply.github.com>
2021-10-26 21:46:07 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_3/Q5_Random/main.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include "Random.h"
+using namespace std;
+
+
+
+int main()
+{
+	Random r;
+	cout << "-- 0에서 " << RAND_MAX << "까지의 랜덤 정수 10개--" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		int n = r.next();
+		cout << n << ' ';
+	}
+
+	cout << endl << endl << "-- 2에서 " << "4 까지의 랜덤 정수 10개 --" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		int n = r.nextInRange(2, 4);
+		cout << n << ' ';
+	}
+	cout << endl;
+
+}
+
