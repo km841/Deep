e3b6b476973098f21ff7672d23b7c13b22ca96d2
km841 <56809638+km841@users.noreply.github.com>
2021-10-26 21:45:51 +0900

    Create Random.h

---
+++ LUXURY C++/Chapter_3/Q5_Random/Random.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Random
+{
+
+private:
+
+
+
+public:
+	Random();
+	int next();
+	int nextInRange(int min, int max);
+
+
+
+};
+
