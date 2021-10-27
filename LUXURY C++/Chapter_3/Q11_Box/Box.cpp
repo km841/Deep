caf92b0b55d9cb752c4926edc39b3932c0771660
km841 <56809638+km841@users.noreply.github.com>
2021-10-27 23:09:26 +0900

    Create Box.cpp

---
+++ LUXURY C++/Chapter_3/Q11_Box/Box.cpp
@@ -0,0 +1,13 @@
+#include "Box.h"
+
+Box::Box(int w, int h) { setSize(w, h); fill = '*'; }
+void Box::setFill(char f) { fill = f; }
+void Box::setSize(int w, int h) { width = w; height = h; }
+
+void Box::draw() {
+	for (int n = 0; n < height; n++)
+	{
+		for (int m = 0; m < width; m++) cout << fill;
+		cout << endl;
+	}
+}
