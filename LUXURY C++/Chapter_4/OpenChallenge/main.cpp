a09d2e7b6ad3e235fb6f26351835d8d1cb1e8b03
km841 <56809638+km841@users.noreply.github.com>
2021-10-30 11:57:41 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_4/OpenChallenge/main.cpp
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include "WordGame.h"
+using namespace std;
+
+int main() 
+{
+	cout << "끝말 잇기 게임을 시작합니다." << endl;
+	cout << "게임에 참가하는 인원은 몇명입니까? ";
+	
+	int n = 0;
+	cin >> n;
+	cin.ignore();
+
+	if (n <= 1) {
+		cout << "혼자서는 게임을 할 수 없습니다!" << endl;
+		return 0;
+	}
+
+	WordGame wg(n);
+	wg.NameSetting();
+	string startWord = wg.StartGame();
+	cout << startWord << endl;
+
+	while (wg.WordCompare()) {}
+
+	cout << "게임이 끝났습니다!" << endl;
+}
+
