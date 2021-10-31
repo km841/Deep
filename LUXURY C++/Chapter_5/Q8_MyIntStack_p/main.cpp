12a2072ad0a68469716d7b4d7653fd34b28545ad
km841 <56809638+km841@users.noreply.github.com>
2021-10-31 19:05:30 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_5/Q8_MyIntStack_p/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "MyIntStack.h"
+using namespace std;
+
+
+
+int main()
+{
+	MyIntStack a(10);
+	a.push(10);
+	a.push(20);
+	MyIntStack b = a;
+	b.push(30);
+
+	int n;
+	a.pop(n);
+	cout << "스택 a에서 팝한 값 " << n << endl;
+	b.pop(n);
+	cout << "스택 b에서 팝한 값 " << n << endl;
+}
+
