8b8045f35e9a6f4d970e5acf4d70d1f4da2fed2b
km841 <56809638+km841@users.noreply.github.com>
2021-10-31 12:00:43 +0900

    Create GamblingGame.cpp

---
+++ LUXURY C++/Chapter_4/Q14_GamblingGame/GamblingGame.cpp
@@ -0,0 +1,55 @@
+#include "GamblingGame.h"
+
+GamblingGame::GamblingGame()
+{
+	_players = new Player[MAX_USER];
+}
+
+GamblingGame::~GamblingGame()
+{
+	if (_players != nullptr)
+	{
+		delete[] _players;
+		_players = nullptr;
+	}
+}
+
+void GamblingGame::SetPlayerName()
+{
+	for (int i = 0; i < MAX_USER; i++)
+	{
+		if (i == 0) cout << "첫 번째 선수 이름>>";
+		else cout << "두 번째 선수 이름>>";
+
+		string name;
+		getline(cin, name);
+
+		_players[i].SetName(name);
+	}
+}
+
+bool GamblingGame::NextTurn(int n)
+{
+	cout << _players[n].GetName() << ":" << "<Enter>";
+	while (getchar() != '\n') {};
+
+	int* checkNum = _players[n].SetNumbers();
+
+	cout << '\t' << '\t';
+
+	for (int i = 0; i < MAX_NUMS; i++)
+	{
+		cout << checkNum[i] << '\t';
+	}
+
+	if (checkNum[0] == checkNum[1] && checkNum[1] == checkNum[2]) {
+		cout << _players[n].GetName() << "님의 승리!!" << endl;
+		return false;
+	}
+
+	else {
+		cout << "아쉽군요!" << endl;
+		return true;
+	}
+}
+
