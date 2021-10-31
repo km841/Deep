169bb201bf03ffdb4f5f523b063b4ee784eb1c3f
km841 <56809638+km841@users.noreply.github.com>
2021-10-31 18:52:54 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_5/Q7_MyIntStack/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include "MyIntStack.h"
+using namespace std;
+
+
+
+int main()
+{
+	MyIntStack a;
+	for (int i = 0; i < 11; i++) {
+		if (a.push(i)) cout << i << ' ';
+		else cout << endl << i + 1 << " 번째 stack full" << endl;
+	}
+
+	int n;
+	for (int i = 0; i < 11; i++) {
+		if (a.pop(n)) cout << n << ' ';
+		else cout << endl << i + 1 << " 번째 stack empty";
+	}
+	
+	cout << endl;
+}
+
