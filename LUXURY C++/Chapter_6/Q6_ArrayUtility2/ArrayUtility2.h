bea68638f0dd7a2b8d579a791968c973236562e5
km841 <56809638+km841@users.noreply.github.com>
2021-11-02 23:20:48 +0900

    Create ArrayUtility2.h

---
+++ LUXURY C++/Chapter_6/Q6_ArrayUtility2/ArrayUtility2.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class ArrayUtility2
+{
+public:
+	static int* concat(int s1[], int s2[], int size);
+	static int* remove(int s1[], int s2[], int size, int& retSize);
+};
+
