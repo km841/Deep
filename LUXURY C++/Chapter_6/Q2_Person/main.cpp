da16454de3a45d7bcfb68389a409c91282c72c29
km841 <56809638+km841@users.noreply.github.com>
2021-11-02 12:43:14 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_6/Q2_Person/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include "Person.h"
+using namespace std;
+
+
+
+int main()
+{
+	Person grace, ashley(2, "Ashley"), helen(3, "Helen", 32.5);
+	grace.show();
+	ashley.show();
+	helen.show();
+
+	
+
+}
