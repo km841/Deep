12c220ca5912653ddf35c87a78b67dfc1b0dab24
km841 <56809638+km841@users.noreply.github.com>
2021-11-02 22:30:45 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_6/Q5_ArrayUtility/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include "ArrayUtility.h"
+using namespace std;
+
+
+int main()
+{
+	int x[] = { 1, 2, 3, 4, 5 };
+	double y[5];
+	double z[] = { 9.9, 8.8, 7.7, 6.6, 5.6 };
+	
+	ArrayUtility::intToDouble(x, y, 5);
+	for (int i = 0; i < 5; i++) cout << y[i] << ' ';
+	cout << endl;
+
+	ArrayUtility::doubleToInt(z, x, 5);
+	for (int i = 0; i < 5; i++) cout << x[i] << ' ';
+	cout << endl;
+	
+
+}
