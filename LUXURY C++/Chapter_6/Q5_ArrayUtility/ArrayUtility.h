57af4c4b2b714a880843d9e53738225ba8dccbd5
km841 <56809638+km841@users.noreply.github.com>
2021-11-02 22:30:34 +0900

    Create ArrayUtility.h

---
+++ LUXURY C++/Chapter_6/Q5_ArrayUtility/ArrayUtility.h
@@ -0,0 +1,8 @@
+#pragma once
+class ArrayUtility
+{
+public:
+	static void intToDouble(int source[], double dest[], int size);
+	static void doubleToInt(double source[], int dest[], int size);
+};
+
