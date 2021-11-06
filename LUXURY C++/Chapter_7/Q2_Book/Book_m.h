d9ab296a9cfa6af807291245712ba2575bf308c8
km841 <56809638+km841@users.noreply.github.com>
2021-11-06 11:37:44 +0900

    Create Book_m.h

---
+++ LUXURY C++/Chapter_7/Q2_Book/Book_m.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Book
+{
+	string title;
+	int price, pages;
+
+public:
+	Book(string title = "", int price = 0, int pages = 0);
+	void show();
+	string getTitle();
+
+	friend const Book& operator+= (Book& b, int n);
+	friend const Book& operator-= (Book& b, int n);
+	bool operator== (int price);
+	bool operator== (string title);
+	bool operator== (const Book& book);
+
+};
+
