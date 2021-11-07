cf4890a158bc2d9b11c322ec7b566760e0db71e9
km841 <56809638+km841@users.noreply.github.com>
2021-11-07 16:27:53 +0900

    Create main.cpp

---
+++ LUXURY C++/Chapter_8/Q1_Circle/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include "pch.h"
+#include "NameCircle.h"
+
+using namespace std;
+
+int main()
+{
+    NameCircle waffle(3, "waffle");
+    waffle.show();
+
+}
+
