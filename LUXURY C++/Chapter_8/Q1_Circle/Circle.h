5f6862964eb1336a250f616bb6e71d3cfc8b886f
km841 <56809638+km841@users.noreply.github.com>
2021-11-07 16:27:14 +0900

    Create Circle.h

---
+++ LUXURY C++/Chapter_8/Q1_Circle/Circle.h
@@ -0,0 +1,11 @@
+#pragma once
+class Circle
+{
+	int radius;
+public:
+	Circle(int radius = 0) { this->radius = radius; }
+	int getRadius() { return radius; }
+	void setRadius(int radius) { this->radius = radius; }
+	double getArea() { return 3.14 * radius * radius; }
+};
+
