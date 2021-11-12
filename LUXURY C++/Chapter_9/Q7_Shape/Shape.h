7d42db139555e3a95005aa741376ab5bee10a232
km841 <56809638+km841@users.noreply.github.com>
2021-11-12 21:56:16 +0900

    Create Shape.h

---
+++ LUXURY C++/Chapter_9/Q7_Shape/Shape.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape
+{
+protected:
+	string name;
+	int width, height;
+
+public:
+	Shape(string n = "", int w = 0, int h = 0) { name = n; width = w; height = h; }
+	virtual double getArea() { return 0; }
+	string getName() { return name; }
+};
+
