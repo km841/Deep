f0ca3e6d584f15bbda431a0b48a96849b8104afe
km841 <56809638+km841@users.noreply.github.com>
2021-11-14 19:21:11 +0900

    Create WordGame.h

---
+++ LUXURY C++/Chapter_10/Q12_WordGame2/WordGame.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "Word.h"
+class WordGame
+{
+	vector<Word> wordBook;
+	size_t wordSize;
+
+public:
+	WordGame();
+	void init();
+	void add(string kor, string eng);
+	bool Questions();
+	bool AddWord();
+
+};
+
