7b789e5bf0e91139d8999d4890f7eb1c1a94269e
km841 <56809638+km841@users.noreply.github.com>
2021-11-28 10:53:01 +0900

    Create 3-5.cpp

---
+++ WindowsProgramming/Chapter_3/Practice/3-5.cpp
@@ -0,0 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <iostream>
+#include <tchar.h>
+
+#define BSIZE 40
+
+double LengthPts(int x1, int y1, int x2, int y2) {
+	return (sqrt((float)((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))));
+}
+
+BOOL InCircle(int x, int y, int mx, int my) {
+	if (LengthPts(x, y, mx, my) < BSIZE) return TRUE;
+	else return FALSE;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrev, LPSTR lpszCmdLine, int nCmdShow)
+{
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS WndClass;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = _T("Window Class Name");
+
+	RegisterClass(&WndClass);
+
+	hwnd = CreateWindow(_T("Window Class Name"),
+		_T("확씨의 첫 번째 윈도우"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL);
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	static int x, y;
+	static BOOL Selection;
+	int mx, my;
+
+	
+	switch (iMsg) {
+	case WM_CREATE:
+		x = 50, y = 50;
+		Selection = FALSE;
+		break;
+
+
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		if (Selection)
+			Rectangle(hdc, x - BSIZE, y - BSIZE, x + BSIZE, y + BSIZE);
+		Ellipse(hdc, x - BSIZE, y - BSIZE, x + BSIZE, y + BSIZE);
+
+		EndPaint(hwnd, &ps);
+		break;
+
+	case WM_LBUTTONDOWN:
+		mx = LOWORD(lParam);
+		my = HIWORD(lParam);
+		if (InCircle(x, y, mx, my)) Selection = TRUE;
+		InvalidateRgn(hwnd, NULL, TRUE);
+		break;
+
+	case WM_LBUTTONUP:
+		Selection = FALSE;
+		InvalidateRgn(hwnd, NULL, TRUE);
+		break;
+			
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
