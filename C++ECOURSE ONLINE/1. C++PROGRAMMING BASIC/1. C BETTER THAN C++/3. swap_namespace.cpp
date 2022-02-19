c6a9966931e86b03452922ffa142936f4d8b4508
Deep <56809638+km841@users.noreply.github.com>
2022-02-19 10:43:26 +0900

    Create 3. swap_namespace.cpp

---
+++ C++ECOURSE ONLINE/1. C++PROGRAMMING BASIC/1. C BETTER THAN C++/3. swap_namespace.cpp
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+namespace Util {
+	template <typename T>
+	void Swap(T& x, T& y) {
+		T temp = x;
+		x = y;
+		y = temp;
+	}
+}
+
+int main()
+{
+	int n= 1, m = 2;
+	double i = 1.1, j = 2.2;
+	cout << n << ", " << m << endl;
+
+	Util::Swap(n, m);
+	cout << n << ", " << m << endl;
+
+	cout << i << ", " << j << endl;
+
+	Util::Swap(i, j);
+	cout << i << ", " << j << endl;
+
+
+	return 0;
+}
