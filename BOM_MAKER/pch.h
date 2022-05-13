96f9b888da58c43df3e88f78c928fbd09aae8544
Deep <56809638+km841@users.noreply.github.com>
2022-05-13 15:23:12 +0900

    Create pch.h

---
+++ BOM_MAKER/pch.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <vector>
+#include <string>
+#include <filesystem>
+#include <thread>
+#include <future>
+#include <variant>
+#include <tchar.h>
+#include <chrono>
+
+#include "resource.h"
+#include "boost/lexical_cast.hpp"
+#include "boost/format.hpp"
+#include "xlnt/xlnt.hpp"
+
+
+
+namespace fs = std::filesystem;
+class pch
+{
+};
+
